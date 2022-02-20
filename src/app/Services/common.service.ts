07c7d37f3650d256efefd1f55e07b10e4f594f6e
Tien Tran <hoangtien103@gmail.com>
2022-02-20 19:59:31 +0700

    service

---
+++ src/app/Services/common.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CommonService {
+  public counter = 2;
+  constructor() {}
+
+  public binhPhuong(n: number): number {
+    return n * n;
+  }
+}
